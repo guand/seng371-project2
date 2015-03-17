6965eda324e464b021313336095ba82afaad115a
Paul Moon <moon.haein@gmail.com>
2015-03-17 11:37:09 -0700

    Added scripts for querying GitHub, and the framework for the scripts/website.

---
+++ scripts/github.js
@@ -0,0 +1,149 @@
+var GitHubAPI = require('github');
+// Object to URL query string
+var QS = require('qs');
+// Promises
+var Q = require('q');
+// Underscore.js
+var _ = require('lodash');
+var async = require('async');
+
+
+var github = new GitHubAPI({
+	version: "3.0.0",
+	debug: true,
+	protocol: "https",
+	timeout: 5000,
+	headers: {
+		"user-agent": "SENG371-SentimentAnalysis"
+	}
+});
+
+var currentUser, currentRepo, pullRequests, issues, stars, commits, contributors;
+
+github.authenticate({
+    type: "oauth",
+    token: 'c43b70e87678d01e1906699c7324b9816db81e83',
+});
+
+function getCurrentUser() {
+	return currentUser;
+}
+
+function getCurrentRepo() {
+	return currentRepo;
+}
+
+function getRepoUser(repoName) {
+	var deferred = Q.defer();
+
+	github.search.repos({
+		"q": repoName
+	}, function(error, result) {
+		currentRepo = result.items[0];
+		// console.log(result.items[0]);
+		deferred.resolve(currentRepo.owner.login);
+	});
+
+	return deferred.promise;
+}
+
+
+function getData(repoName) {
+	var deferred = Q.defer();
+
+	getRepoUser(repoName)
+		.then(function (repoUser) {
+			currentUser = repoUser;
+			return getNumPRs(repoUser, repoName);
+		})
+		.then(function (pullRequests) {
+			console.log(pullRequests.length);
+			return getNumIssues(repoUser, repoName);
+		})
+		.then(function (issues) {
+			console.log(issues.length);
+			deferred.resolve();
+		});
+
+	return deferred.promise;
+}
+
+function getNumPRs(ownerName, repoName) {
+	var deferred = Q.defer();
+	var pullRequestsAdded = false;
+	var page = 1;
+	pullRequests = [];
+
+	console.log('Getting PRs');
+
+	async.doWhilst(function (callback) {
+		pullRequestsAdded = false;
+		github.pullRequests.getAll({
+			user: ownerName,
+			repo: repoName,
+			state: "closed", 
+			per_page: 100,
+			page: page
+		}, function(error, result) {
+			pullRequests = pullRequests.concat(_.filter(result, function(pr) {
+				return pr.merged_at !== null;
+			}));
+
+			if (result.meta.link !== undefined && result.meta.link.indexOf('rel="next"') !== -1) {
+				pullRequestsAdded = true;
+				page += 1;
+			}
+			callback();
+		});
+	}, function () {
+		return pullRequestsAdded;
+	}, function (err) {
+		deferred.resolve(pullRequests);
+	});
+
+	return deferred.promise;
+}
+
+function getNumIssues(ownerName, repoName) {
+	var deferred = Q.defer();
+	var issuesAdded = false;
+	var page = 1;
+	issues = [];
+
+	console.log('Getting Issues');
+
+	async.doWhilst(function (callback) {
+		issuesAdded = false;
+		github.issues.repoIssues({
+			user: ownerName,
+			repo: repoName,
+			state: "all",
+			assignee: "*",
+			per_page: 100,
+			page: page
+		}, function(error, result) {
+			issues = issues.concat(result);
+
+			if (result.meta.link !== undefined && result.meta.link.indexOf('rel="next"') !== -1) {
+				issuesAdded = true;
+				page += 1;
+			}
+
+			callback();
+		});
+	}, function () {
+		return issuesAdded;
+	}, function (err) {
+		deferred.resolve(issues);
+	});
+
+	return deferred.promise;
+}
+
+module.exports = {
+	getCurrentRepo : getCurrentRepo,
+	getRepoUser : getRepoUser,
+	getData : getData,
+	getNumPRs : getNumPRs,
+	getNumIssues : getNumIssues,
+}
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var jade = require('jade');
+var Q = require('q');
 var d3 = require('d3');
 var Snoocore = require('snoocore');
+
+var githubScripts = require('./scripts/github.js');
 
 var reddit = new Snoocore({
   userAgent: 'SENG371-SentimentAnalysis@0.0.1 by seng371-sentimental'  
@@ -14,9 +17,18 @@ var port = process.env.PORT || 9001;
 var router = express.Router();
 
 router.get('/', function(request, result) {
-	result.render('index', 
-		{title: 'SENG371 Project #2 - Sentimental Analysis with GitHub Activities'}
-	);
+    result.render('index', 
+        {title: 'SENG371 Project #2 - Sentimental Analysis with GitHub Activities'}
+    );
+});
+
+router.get('/repo', function(request, result) {
+    result.render('index', 
+        {title: 'SENG371 Project #2 - Sentimental Analysis with GitHub Activities'}
+    );
+
+    var currentRepo = request.query.repoName;
+    githubScripts.getData(currentRepo);
 });
 
 app.set('views', __dirname + '/views');
