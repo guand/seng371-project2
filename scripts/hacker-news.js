f5bbbdb43fd1cf26a3ecbc5865c1ee85e2cf8b28
Paul Moon <moon.haein@gmail.com>
2015-04-09 16:09:42 -0700

    Simplified math behind chart margins. Parallelized HN data retrievals. Enabled saving data to files, and retrieving JSON via a URL.

--- scripts/hacker-news.js
+++ scripts/hacker-news.js
@@ -1,77 +1,119 @@
-var http = require('http');
-
+var async = require('async');
+var Q = require('q');
 /*npm install hacker-news-api*/
 var hn = require('hacker-news-api');
 /*npm install sentiment*/
 var sentiment = require('sentiment');
 
 
-var repoName = "Bootstrap";
+var search = function(repoName) {
+  console.log('Searching HackerNews...');
 
-http.createServer(function(request, response) {
-	// response.writeHead(200, {"Content-Type": "application/json"});
-	var commentScores = [];
-	var storyScores = [];
+  var deferred = Q.defer();
 
-	var commentsDate = [];
-	var storysDate = [];
+  async.parallel({
+    comments_since_past_month: function(callback) {
+      var comments = {};
+      comments.dates = [];
+      comments.scores = [];
 
-	// Search for comments containing repoName since past month
-	hn.comment().search(repoName).since('past_month').top(function (error, data) {
-	 
-		if (error) throw error ;
-		for (var i = 0; i < data.hits.length; i++) {
-			commentsDate.push(JSON.stringify(data.hits[i].created_at.split("T")[0]));
+      // Search for comments containing repoName since past month
+      hn.comment().search(repoName).since('past_month').top(function (error, data) {
+        if (error) throw error;
+        for (var i = 0; i < data.hits.length; i++) {
+          comments.dates.push(data.hits[i].created_at.split("T")[0]);
 
-			var r1 = sentiment(JSON.stringify(data.hits[i].comment_text));
-			commentScores.push(JSON.stringify(r1.score));
-		}
-	});
+          var r1 = sentiment(data.hits[i].comment_text);
+          comments.scores.push(r1.score);
+        }
+        callback(null, comments);
+      });
+    },
+    comments_before_past_month: function(callback) {
+      var comments = {};
+      comments.dates = [];
+      comments.scores = [];
 
-	// Search for comments containing repoName after past month
-	hn.comment().search(repoName).before('past_month').top(function (error, data) {
-	 
-		if (error) throw error ;
-		for (var i = 0; i < data.hits.length; i++) {
-			commentsDate.push(JSON.stringify(data.hits[i].created_at.split("T")[0]));
+      // Search for comments containing repoName after past month
+      hn.comment().search(repoName).before('past_month').top(function (error, data) {
+        if (error) throw error;
+        for (var i = 0; i < data.hits.length; i++) {
+          comments.dates.push(data.hits[i].created_at.split("T")[0]);
 
-			var r1 = sentiment(JSON.stringify(data.hits[i].comment_text));
-			commentScores.push(JSON.stringify(r1.score));
-		}
-	});
+          var r1 = sentiment(data.hits[i].comment_text);
+          comments.scores.push(r1.score);
+        }
+        callback(null, comments);
+      });
 
-	// Search for storys containing repoName since past month
-	hn.story().search(repoName).since('past_month').top(function (error, data) {
-	 
-		if (error) throw error ;
-		for (var i = 0; i < data.hits.length; i++) {
-			if (data.hits[i]._highlightResult.story_text == null || data.hits[i]._highlightResult.story_text.value == "") {
-				var r1 = sentiment(JSON.stringify(data.hits[i]._highlightResult.title.value));
-			}
-			else {
-				var r1 = sentiment(JSON.stringify(data.hits[i]._highlightResult.story_text.value));
-			}
-			storysDate.push(JSON.stringify(data.hits[i].created_at.split("T")[0]));
+    },
+    stories_since_past_month: function(callback) {
+      var stories = {};
+      stories.dates = [];
+      stories.scores = [];
+      
+      // Search for storys containing repoName since past month
+      hn.story().search(repoName).since('past_month').top(function (error, data) {
+        if (error) throw error;
+        for (var i = 0; i < data.hits.length; i++) {
+          if (data.hits[i]._highlightResult.story_text == null || data.hits[i]._highlightResult.story_text.value == "") {
+            var r1 = sentiment(data.hits[i]._highlightResult.title.value);
+          }
+          else {
+            var r1 = sentiment(data.hits[i]._highlightResult.story_text.value);
+          }
+          stories.dates.push(data.hits[i].created_at.split("T")[0]);
+          stories.scores.push(r1.score);
+        }
+        callback(null, stories);
+      });
 
-			storyScores.push(JSON.stringify(r1.score));
-		}
-	});
+    },
+    stories_before_past_month: function(callback) {
+      var stories = {};
+      stories.dates = [];
+      stories.scores = [];
 
-	// Search for storys containing repoName after past month
-	hn.story().search(repoName).before('past_month').top(function (error, data) {
-	 
-		if (error) throw error ;
-		for (var i = 0; i < data.hits.length; i++) {
-			if (data.hits[i]._highlightResult.story_text == null || data.hits[i]._highlightResult.story_text.value == "") {
-				var r1 = sentiment(JSON.stringify(data.hits[i]._highlightResult.title.value));
-			}
-			else {
-				var r1 = sentiment(JSON.stringify(data.hits[i]._highlightResult.story_text.value));
-			}
-			storysDate.push(JSON.stringify(data.hits[i].created_at.split("T")[0]));
-			
-			storyScores.push(JSON.stringify(r1.score));
-		}
-	});
-	// response.end();
-}).listen(8888);
+      // Search for storys containing repoName after past month
+      hn.story().search(repoName).before('past_month').top(function (error, data) {
+        if (error) throw error;
+        for (var i = 0; i < data.hits.length; i++) {
+          if (data.hits[i]._highlightResult.story_text == null || data.hits[i]._highlightResult.story_text.value == "") {
+            var r1 = sentiment(data.hits[i]._highlightResult.title.value);
+          }
+          else {
+            var r1 = sentiment(data.hits[i]._highlightResult.story_text.value);
+          }
+          stories.dates.push(data.hits[i].created_at.split("T")[0]);
+          stories.scores.push(r1.score);
+        }
+        callback(null, stories);
+      });
+    }
+  }, function(err, results) {
+    console.log('finished!');
+
+    var dates = [];
+    var scores = [];
+
+    var array = [results.comments_since_past_month, 
+                 results.comments_before_past_month,
+                 results.stories_since_past_month,
+                 results.stories_before_past_month];
+
+    for (var i = 0; i < array.length; i++) {
+      dates = dates.concat(array[i].dates);
+      scores = scores.concat(array[i].scores);
+    }    
+
+    var returned = {dates: dates, scores: scores};
+
+    deferred.resolve(returned);
+  });
+
+  return deferred.promise;
+}
+
+module.exports = {
+	search: search
+}
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
-var morgan = require('morgan');
-var methodOverride = require('method-override');
 var jade = require('jade');
 var Q = require('q');
 var d3 = require('d3');
-// https://github.com/mbostock/d3/pull/2225#issuecomment-73426201
-var jsdom = require('jsdom');
+var fs = require("fs");
 var hackerNews = require('./scripts/hacker-news.js');
-var redditScript = require('./scripts/reddit');
-var githubScripts = require('./scripts/github.js');
+var reddit = require('./scripts/reddit');
+var gitHub = require('./scripts/github.js');
 
 var app = express();
 var port = process.env.PORT || 9001;
@@ -26,14 +23,42 @@ router.get('/repo', function(request, result) {
     );
 
     var currentRepo = request.query.repoName;
-    var gitHubData, redditData;
+    var repoFileName = currentRepo + '.json';
+    var gitHubData, redditData, hackerNewsData;
     
-    Q.all([githubScripts.getData(currentRepo),
-            redditScript.search(currentRepo)])
-        .spread(function(gitHubData, redditData) {
-            console.log(gitHubData.length);
-            result.send(gitHubData);
-        })
+    hackerNews.search(currentRepo).then(function (response) {
+      fs.writeFile('data/' + repoFileName, JSON.stringify(response, null, 2), function(err) {
+        if (err) {
+          console.log('Could not write to file: ' + repoFileName);
+        }
+
+        console.log('Wrote to file: ' + repoFileName);
+      })
+    });
+
+//    Q.all([gitHub.getData(currentRepo),
+//            reddit.search(currentRepo),
+//            hackerNews.search(currentRepo)])
+//        .spread(function(gitHubData, redditData, hackerNewsData) {
+// 
+//        })
+});
+
+router.get('/data', function(request, result) {
+  var currentRepo = request.query.repoName;
+
+  console.log('looking');
+  console.log(currentRepo);
+
+  fs.readFile('data/' + repoFileName, function(err, data) {
+    if (err) {
+      console.log(err);
+      result.send('Could not find such a file: data/' + repoFileName);
+    } else {
+      result.send(data);
+    }
+  });
+
 });
 
 app.set('views', __dirname + '/views');
@@ -42,4 +67,3 @@ app.use('/', router);
 app.listen(port);
 
 console.log('Listening on port: ' + port);
-
